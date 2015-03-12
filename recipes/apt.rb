008555014b9558be7f5889b06bc92aab3fdbfc9d
nathan <nathan@vertile.com>
2015-03-12 10:51:09 -0700

    use the chef cache path for the apt key

--- recipes/apt.rb
+++ recipes/apt.rb
@@ -1,13 +1,13 @@
 include_recipe "apt"
 
-remote_file "/tmp/tracelytics.key" do
+remote_file "#{Chef::Config[:file_cache_path]}/tracelytics.key" do
     source "https://apt.tracelytics.com/tracelytics-apt-key.pub"
     mode "0644"
     checksum "6c560210a2265cba05edcba6629b8e3383db414bab94a8c89d1369ad5b56691e"
 end
 
 execute "add-apt-key" do
-    command "apt-key add /tmp/tracelytics.key"
+    command "apt-key add #{Chef::Config[:file_cache_path]}/tracelytics.key"
     action :run
     not_if "apt-key list | grep 03311F21"
 end
